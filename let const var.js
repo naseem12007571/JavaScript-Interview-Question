4f5a19b5f1ccfb40d7b99709921078629968f41a
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-05-16 00:07:06 +0530

    Update let const var.js

--- let const var.js
+++ let const var.js
@@ -11,4 +11,21 @@ console.log(a)
 // console.log(n)  here we got referene error
 
 
+// let varibale ko  we can not redclear and cost aslo we can not redclear
+function test(){
+     var a= "naseem"
+     let b= "choudahry"
+     if(true){
+        let a ="raja"
+        //var b=        here let varibale ko redeclear nhi kar sakte in the same scope
+        console.log(a)
+
+     }
+     console.log(a)
+}
+
+test();
+
+
+
 
